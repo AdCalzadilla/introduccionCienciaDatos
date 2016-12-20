990862fc27d0bc83e297f359fdbc620edbac7b91
Adrian <alu0100288216@ull.edu.es>
2016-12-20 10:58:40 +0100

    Resolviendo conflictos.

--- wizRegresion.r
+++ wizRegresion.r
@@ -3,6 +3,29 @@ wizmir <- read.csv2("./wizmir/wizmir.dat", header = F, sep = ",", comment.char =
 # Nombres de las variables
 names(wizmir) <- c("Max_temperature", "Min_temperature", "Dewpoint", "Precipitation", "Sea_level_pressure", "Standard_pressure", "Visibility", "Wind_speed", "Max_wind_speed", "Mean_temperature")
 
+# Creamos varias funciones que utilizaremos a lo largo del código
+
+## Funciones para crear una tabla donde poder comparar los resultados
+
+percentage <- function(x){
+  x * 100
+}
+
+createTable <- function(x){
+  df <- data.frame(cbind(x), stringsAsFactors = F)
+  porcentaje <- sapply(df[,1], as.numeric)
+  porcentaje <- sapply(df[,1], percentage)
+  df <- cbind(df, porcentaje)
+  df
+}
+
+## Función para calcular la moda
+
+getMode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
 # Miramos el tipo de objeto que tenemos y el tipo de sus variables.
 
 ## Características generales de wizmir
@@ -26,7 +49,7 @@ is.double(Wind_speed)
 is.double(Max_wind_speed)
 is.double(Mean_temperature)
 
-## Resumen del data.frame en el que se nos muestra el valor máximo y mínimo registrado, 
+## Resumen del data.frame en el que se nos muestra el valor máximo y mínimo registrado,
 ## el primer cuartil, la mediana, el tercer cuartil y la media de cada variable.
 summary(wizmir)
 
@@ -43,13 +66,6 @@ allIQR <- apply(wizmir, 2, IQR)
 plot(wizmir)
 
 # Análisis de cada una de las variables por separado
-
-## Función para calcular la moda
-
-getMode <- function(v) {
-  uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
-}
 
 ## Max_temperature
 
@@ -247,3 +263,143 @@ max(Mean_temperature)
 min(Mean_temperature)
 range(Mean_temperature)
 quantile(Mean_temperature)
+
+# Regresión lineal
+
+## Max_temperature
+
+lm1 <- lm(Mean_temperature~Max_temperature, data = wizmir)
+summary(lm1)
+plot(Mean_temperature~Max_temperature, wizmir)
+abline(lm1, col="red")
+confint(lm1)
+# Lista para ir introduciendo los resultados del r2 ajustado.
+adj.r.squared <- list(lm1=round(summary(lm1)$adj.r.squared, digits = 4))
+
+## Min_temperature
+
+lm2 <- lm(Mean_temperature~Min_temperature, data = wizmir)
+summary(lm2)
+plot(Mean_temperature~Min_temperature, wizmir)
+abline(lm2, col="red")
+confint(lm2)
+adj.r.squared <- c(adj.r.squared, lm2=round(summary(lm2)$adj.r.squared, digits = 4))
+
+## Dewpoint
+
+lm3 <- lm(Mean_temperature~Dewpoint, data = wizmir)
+summary(lm3)
+plot(Mean_temperature~Dewpoint, wizmir)
+abline(lm3, col= "red")
+confint(lm3)
+adj.r.squared <- c(adj.r.squared, lm3=round(summary(lm3)$adj.r.squared, digits = 4))
+
+## Sea_level_pressure
+
+lm4 <- lm(Mean_temperature~Sea_level_pressure, data = wizmir)
+summary(lm4)
+plot(Mean_temperature~Sea_level_pressure, wizmir)
+abline(lm4, col = "red")
+confint(lm4)
+adj.r.squared <- c(adj.r.squared, lm4=round(summary(lm4)$adj.r.squared, digits = 4))
+
+## Standard_pressure
+
+lm5 <- lm(Mean_temperature~Standard_pressure, data = wizmir)
+summary(lm5)
+plot(Mean_temperature~Standard_pressure, data = wizmir)
+abline(lm5, col = "red")
+confint(lm5)
+adj.r.squared <- c(adj.r.squared, lm5=round(summary(lm5)$adj.r.squared, digits = 4))
+
+## Conclusiones (Tabla con todos los r2 ajustados)
+
+createTable(adj.r.squared)
+
+# Modelo lineal múltiple
+
+## Max_temperature y Min_temperature (Mejor aproximación por separado)
+
+lm6 <- lm(Mean_temperature~Max_temperature+Min_temperature, data = wizmir)
+summary(lm6)
+adj.r.squared <- c(adj.r.squared, lm6=round(summary(lm6)$adj.r.squared, digits = 4))
+
+## Modelo con todas las variables
+
+lm7 <- lm(Mean_temperature~., data = wizmir)
+summary(lm7)
+adj.r.squared <- c(adj.r.squared, lm7=round(summary(lm7)$adj.r.squared, digits = 4))
+
+## Eliminación de las variables no relevantes
+
+lm8 <- lm(Mean_temperature~.-Precipitation-Wind_speed, data = wizmir)
+summary(lm8)
+adj.r.squared <- c(adj.r.squared, lm8=round(summary(lm8)$adj.r.squared, digits = 4))
+
+lm9 <- lm(Mean_temperature~.-Precipitation-Wind_speed-Standard_pressure-Max_wind_speed, data = wizmir)
+summary(lm9)
+adj.r.squared <- c(adj.r.squared, lm9=round(summary(lm9)$adj.r.squared, digits = 4))
+
+lm10 <- lm(Mean_temperature~.-Precipitation-Wind_speed-Standard_pressure-Max_wind_speed-Dewpoint, data = wizmir)
+summary(lm10)
+adj.r.squared <- c(adj.r.squared, lm10=round(summary(lm10)$adj.r.squared, digits = 4))
+
+## Conclusiones (Tabla con todos los r2 ajustados)
+
+createTable(adj.r.squared)
+
+# Interacciones (Todos con todos)
+
+## Max_temperature * Min_temperature
+lm11 <- lm(Mean_temperature~Max_temperature*Min_temperature, data = wizmir)
+summary(lm11)
+adj.r.squared <- c(adj.r.squared, lm11=round(summary(lm11)$adj.r.squared, digits = 4))
+
+# Max_temperature * Dewpoint
+lm12 <- lm(Mean_temperature~Max_temperature*Dewpoint, data = wizmir)
+summary(lm12)
+adj.r.squared <- c(adj.r.squared, lm12=round(summary(lm12)$adj.r.squared, digits = 4))
+
+# Max_temperature * Sea_level_pressure
+lm13 <- lm(Mean_temperature~Max_temperature*Sea_level_pressure, data = wizmir)
+summary(lm13)
+adj.r.squared <- c(adj.r.squared, lm13=round(summary(lm13)$adj.r.squared, digits = 4))
+
+# Max_temperature * Standard_pressure
+lm14 <- lm(Mean_temperature~Max_temperature*Standard_pressure, data = wizmir)
+summary(lm14)
+adj.r.squared <- c(adj.r.squared, lm14=round(summary(lm14)$adj.r.squared, digits = 4))
+
+# Min_temperature * Dewpoint
+lm15 <- lm(Mean_temperature~Min_temperature*Dewpoint, data = wizmir)
+summary(lm15)
+adj.r.squared <- c(adj.r.squared, lm15=round(summary(lm15)$adj.r.squared, digits = 4))
+
+# Min_temperature * Sea_level_pressure
+lm16 <- lm(Mean_temperature~Min_temperature*Sea_level_pressure, data = wizmir)
+summary(lm16)
+adj.r.squared <- c(adj.r.squared, lm16=round(summary(lm16)$adj.r.squared, digits = 4))
+
+# Min_temperature * Standard_pressure
+lm17 <- lm(Mean_temperature~Min_temperature*Standard_pressure, data = wizmir)
+summary(lm17)
+adj.r.squared <- c(adj.r.squared, lm17=round(summary(lm17)$adj.r.squared, digits = 4))
+
+# Dewpoint * Sea_level_pressure
+lm18 <- lm(Mean_temperature~Dewpoint*Sea_level_pressure, data = wizmir)
+summary(lm18)
+adj.r.squared <- c(adj.r.squared, lm18=round(summary(lm18)$adj.r.squared, digits = 4))
+
+# Dewpoint * Standard_pressure
+lm19 <- lm(Mean_temperature~Dewpoint*Standard_pressure, data = wizmir)
+summary(lm19)
+adj.r.squared <- c(adj.r.squared, lm19=round(summary(lm19)$adj.r.squared, digits = 4))
+
+# Sea_level_pressure * Standard_pressure
+lm20 <- lm(Mean_temperature~Sea_level_pressure*Standard_pressure, data = wizmir)
+summary(lm20)
+adj.r.squared <- c(adj.r.squared, lm20=round(summary(lm20)$adj.r.squared, digits = 4))
+
+## Conclusiones (Tabla con todos los r2 ajustados)
+
+createTable(adj.r.squared)
